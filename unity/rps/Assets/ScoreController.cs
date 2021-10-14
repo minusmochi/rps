3a945462c1232eae852178be46fae959fbe55213
Stuart McGillivray <stuart.mcg@hotmail.co.uk>
2021-10-14 19:58:53 +0100

    Adding basics of an event system

---
+++ unity/rps/Assets/ScoreController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    private int _score = 0;
+
+    public void Start()
+    {
+        GameEventSystem.Current.onIncreaseScore += OnIncreseScore;
+    }
+
+    private void OnIncreseScore()
+    {
+        Debug.Log("Increasing the score");
+        var scoreArea = GetComponent<TextMeshProUGUI>();
+
+        _score += 1;
+
+        scoreArea.text = _score.ToString();
+    }
+}
+
